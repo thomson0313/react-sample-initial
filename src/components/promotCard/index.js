3cb2e29a89e78017476943e40b7c1ebf2196b468
thomson0313 <thomson19960313@gmail.com>
2024-07-31 18:58:13 +1000

    feat:combination of 2 img

--- src/components/promotCard/index.js
+++ src/components/promotCard/index.js
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import WalletConnectModal from "../walletConnectModal";
 import './styles.promotCard.scss'
 
-import PromoteImg from '../../assets/img/promote.gif'
+import PromoteImg_1 from '../../assets/img/promote.gif'
+import PromoteImg_2 from '../../assets/img/promote.jpg'
 import Logo from '../../assets/img/logo.png'
 import CloseButton from '../../assets/img/close.svg'
+
+const PromoteImgs = [PromoteImg_1, PromoteImg_2];
 
 const PromotCard = () => {
     const [isOpen, setIsopen] = useState(true);
     const [isModal, setIsModal] = useState(false);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
     const handleClick = () => {
         setIsopen(!isOpen)
@@ -19,12 +23,20 @@ const PromotCard = () => {
         setIsModal(flag);
     }
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % PromoteImgs.length);
+        }, 3000); // Change image every 3 seconds
+
+        return () => clearInterval(interval); // Cleanup interval on component unmount
+    }, []);
+
     return(
         <div>
             {isOpen?
                 <div className="promot-card">
                     <div className="video-section">
-                        <img src={PromoteImg} alt="img" />
+                        <img src={PromoteImgs[currentIndex]} alt="img" />
                     </div>
                     <div className="contents">Get 100 $REDY - swap $10!</div>
                     <div className="claim-button" onClick={() => setIsModal(true)}>Claim now</div>
